8846b517a2d2a456c506b56419e1d251a763d094
Szuhi <szuhanszky.tamas@gmail.com>
2017-11-01 19:41:33 +0100

    307_Pong, Pong Game
    Final commit,
    
    Space - Add new ball
    Left and Right Arrow - Move the slider

---
+++ 307_Pong/Pong/Pong/Classes.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Pong
+{
+    public class ViewModel : Bindable
+    {
+        public ViewModel()
+        {
+            Balls = new List<Sphere>();
+            Slider = new Bat();
+            Lifes = 5;
+        }
+
+        int lifes;
+        public List<Sphere> Balls { get; set; }
+        public Bat Slider { get; set; }
+        public int Lifes { get { return lifes; } set { lifes = value; OPC(); } }
+    }
+
+    public class Sphere : Bindable
+    {
+        static Random rnd = new Random();
+        int dx, dy;
+        public Sphere()
+        {
+            dx = 3; dy = 2;
+            Init();
+        }
+
+        Rect shape;
+        public Rect Shape { get { return shape; } set { shape = value; OPC(); } }
+        public Brush Colour { get; set; }
+
+        public bool Move(int ch, int cw, Bat u)
+        {
+            Shape = new Rect(Shape.X + dx, Shape.Y + dy, Shape.Height, Shape.Width);
+            if (Shape.Top <= 0 || Shape.IntersectsWith(u.Shape))
+                dy = -dy;
+            if (Shape.Left <= 0 || Shape.Right >= cw)
+                dx = -dx;
+            if (Shape.Top >= ch)                                      
+                return true;
+            return false;
+        }
+
+        public void Init()
+        {
+            Shape = new Rect(100, 100, 40, 40);
+            Colour = new SolidColorBrush(Color.FromRgb((byte)rnd.Next(0, 256), (byte)rnd.Next(0, 256), (byte)rnd.Next(0, 256)));
+        }
+    }
+
+    public class Bat : Bindable
+    {
+        public Bat()
+        {
+            Shape = new Rect(300, 300, 100, 10);
+        }
+
+        Rect shape;
+        public Rect Shape { get { return shape; } set { shape = value; OPC(); } }
+
+        public void Move(int dx, int cw)
+        {
+            if (Shape.Left + dx >= 0 && Shape.Right + dx <= cw)
+                Shape = new Rect(dx + Shape.X, Shape.Y, Shape.Width, Shape.Height);
+        }
+    }
+
+    public abstract class Bindable : INotifyPropertyChanged
+    {
+        protected void OPC([CallerMemberName] string s = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(s));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
--- 307_Pong/Pong/Pong/MainWindow.xaml.cs
+++ 307_Pong/Pong/Pong/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Pong
 {
@@ -20,9 +21,60 @@ namespace Pong
     /// </summary>
     public partial class MainWindow : Window
     {
+        ViewModel VM;
         public MainWindow()
         {
             InitializeComponent();
+            VM = new ViewModel();
+            this.DataContext = VM;
+            KeyDown += MainWindow_KeyDown;
+        }
+
+        void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left: VM.Slider.Move(-5, (int)(this.Content as Canvas).ActualWidth); break;
+                case Key.Right: VM.Slider.Move(+5, (int)(this.Content as Canvas).ActualWidth); break;
+                case Key.Space: AddBall(); break;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer dt = new DispatcherTimer();
+            dt.Interval = TimeSpan.FromMilliseconds(20);
+            dt.Tick += dt_Tick;
+            dt.Start();
+            AddBall();
+        }
+
+        void dt_Tick(object sender, EventArgs e)
+        {
+            foreach (Sphere s in VM.Balls)
+            {
+                if (s.Move((int)(this.Content as Canvas).ActualHeight, (int)(this.Content as Canvas).ActualWidth, VM.Slider))
+                {
+                    VM.Lifes--;
+                    s.Init();
+                }
+            }
+        }
+
+        void AddBall()
+        {
+            Ellipse e = new Ellipse();
+            Sphere s = new Sphere();
+
+            e.DataContext = s;                  
+            e.SetBinding(Canvas.LeftProperty, new Binding("Shape.X"));
+            e.SetBinding(Canvas.TopProperty, new Binding("Shape.Y"));
+            e.SetBinding(Ellipse.WidthProperty, new Binding("Shape.Width"));
+            e.SetBinding(Ellipse.HeightProperty, new Binding("Shape.Height"));
+            e.SetBinding(Ellipse.FillProperty, new Binding("Colour"));
+
+            VM.Balls.Add(s);
+            (this.Content as Canvas).Children.Add(e);
         }
     }
 }
