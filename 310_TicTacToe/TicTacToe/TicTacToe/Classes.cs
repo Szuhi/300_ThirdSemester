cf7e1e217668c3c4befefc5d50b640ebe9cd5103
Szuhi <szuhanszky.tamas@gmail.com>
2017-11-02 22:38:34 +0100

    310_TicTacToe, Final game
    Final commit

---
+++ 310_TicTacToe/TicTacToe/TicTacToe/Classes.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace TicTacToe
+{
+    enum FieldType { Empty, X, O };
+
+    class Field
+    {
+        public const int N = 3;
+        FieldType[,] m;
+
+        public Field()
+        {
+            m = new FieldType[N, N];
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < N; j++)
+                    m[i, j] = FieldType.Empty;
+        }
+
+        public FieldType Get(int x, int y)
+        {
+            return m[y, x];
+        }
+
+        public void Set(int x, int y, FieldType ft)
+        {
+            m[y, x] = ft;
+        }
+    }
+
+    class Game : Bindable
+    {
+        public delegate void GameEndsEventHnadler(bool equal);
+        public event GameEndsEventHnadler GameEnds;
+
+        public FieldType Player { get; private set; }
+        public Field Fields { get; private set; }
+
+        bool end;
+
+        public Game()
+        {
+            Player = FieldType.X;
+            Fields = new Field();
+            end = false;
+        }
+
+        public void step(int x, int y)
+        {
+            if (end)
+                return;
+            if (x >= Field.N || y >= Field.N)
+                return;
+            if (Fields.Get(x, y) != FieldType.Empty)
+                return;
+            Fields.Set(x, y, Player);
+            OnPropChanged("Fields");
+
+            if (SomeoneWon())
+            {
+                if (GameEnds != null)
+                    GameEnds(false);
+                end = true;
+            }
+            else if (FieldIsFull())
+            {
+                if (GameEnds != null)
+                    GameEnds(true);
+            }
+            else
+            {
+                SwitchPlayer();
+            }
+
+        }
+
+        void SwitchPlayer()
+        {
+            Player = (Player == FieldType.X) ? FieldType.O : FieldType.X;
+        }
+
+        bool SomeoneWon()
+        {
+            if (Fields.Get(0, 0) != FieldType.Empty && Fields.Get(0, 0) == Fields.Get(1, 0) && Fields.Get(1, 0) == Fields.Get(2, 0))
+                return true;
+            if (Fields.Get(0, 1) != FieldType.Empty && Fields.Get(0, 1) == Fields.Get(1, 1) && Fields.Get(1, 1) == Fields.Get(2, 1))
+                return true;
+            if (Fields.Get(0, 2) != FieldType.Empty && Fields.Get(0, 2) == Fields.Get(1, 2) && Fields.Get(1, 2) == Fields.Get(2, 2))
+                return true;
+            if (Fields.Get(0, 0) != FieldType.Empty && Fields.Get(0, 0) == Fields.Get(0, 1) && Fields.Get(0, 1) == Fields.Get(0, 2))
+                return true;
+            if (Fields.Get(1, 0) != FieldType.Empty && Fields.Get(1, 0) == Fields.Get(1, 1) && Fields.Get(1, 1) == Fields.Get(1, 2))
+                return true;
+            if (Fields.Get(2, 0) != FieldType.Empty && Fields.Get(2, 0) == Fields.Get(2, 1) && Fields.Get(2, 1) == Fields.Get(2, 2))
+                return true;
+            if (Fields.Get(0, 0) != FieldType.Empty && Fields.Get(0, 0) == Fields.Get(1, 1) && Fields.Get(1, 1) == Fields.Get(2, 2))
+                return true;
+            if (Fields.Get(2, 0) != FieldType.Empty && Fields.Get(2, 0) == Fields.Get(1, 1) && Fields.Get(1, 1) == Fields.Get(0, 2))
+                return true;
+            return false;
+        }
+
+        bool FieldIsFull()
+        {
+            for (int i = 0; i < Field.N; i++)
+                for (int j = 0; j < Field.N; j++)
+                    if (Fields.Get(i, j) == FieldType.Empty)
+                        return false;
+            return true;
+        }
+    }
+
+    class ViewModel
+    {
+        public Game BL { get; set; }
+
+        public ViewModel()
+        {
+            BL = new Game();
+        }
+    }
+
+    abstract class Bindable : INotifyPropertyChanged
+    {
+        protected void OnPropChanged([CallerMemberName]string s = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(s));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    class FieldToDrawingConverter : IValueConverter
+    {
+        const int TILE = 100;
+        static string[] images = new string[] {
+            @"themes\theme2\n.jpg",
+            @"themes\theme2\x.jpg",
+            @"themes\theme2\o.jpg"
+        };
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Field f = (Field)value;
+            DrawingImage di = new DrawingImage();
+            DrawingGroup dg = new DrawingGroup();
+            for (int i = 0; i < Field.N; i++)
+                for (int j = 0; j < Field.N; j++)
+                {
+                    Brush colour;
+                    colour = new ImageBrush(new BitmapImage(new Uri(images[(int)f.Get(i, j)], UriKind.Relative)));
+                    dg.Children.Add(new GeometryDrawing(colour, new Pen(Brushes.Black, 2), new RectangleGeometry(new Rect(i * TILE, j * TILE, TILE, TILE))));
+                }
+            di.Drawing = dg;
+            return di;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- 310_TicTacToe/TicTacToe/TicTacToe/MainWindow.xaml.cs
+++ 310_TicTacToe/TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,35 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
+        ViewModel VM;
+        Stopwatch sw;
+
         public MainWindow()
         {
             InitializeComponent();
+            VM = new ViewModel();
+            DataContext = VM;
+            VM.BL.GameEnds += BL_GameEnds;
+        }
+
+        void BL_GameEnds(bool equal)
+        {
+            sw.Stop();
+            MessageBox.Show("The game ends!\n" + (equal ? "Draw!." : "Winner: " + VM.BL.Player) + "\nTime: " + (double)sw.ElapsedMilliseconds / 1000 + " mp");
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point p = e.GetPosition((Image)this.Content);
+            int w_3 = (int)(((Image)this.Content).ActualWidth / 3);
+            int h_3 = (int)(((Image)this.Content).ActualHeight / 3);
+            VM.BL.step((int)p.X / w_3, (int)p.Y / h_3);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            sw = new Stopwatch();
+            sw.Start();
         }
     }
 }
